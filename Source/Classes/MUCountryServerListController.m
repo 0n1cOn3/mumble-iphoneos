ddc972ff2d6a9cedfdb920c04d323c9e4b4aff0b
0n1cOn3 <0n1cOn3@gmx.ch>
2025-06-05 01:46:08 +0200

    Fix connectToHostname typo

--- Source/Classes/MUCountryServerListController.m
+++ Source/Classes/MUCountryServerListController.m
@@ -214,7 +214,7 @@
     
     if (buttonIndex == 1) {
         MUConnectionController *connCtrlr = [MUConnectionController sharedController];
-        [connCtrlr connetToHostname:[serverItem objectForKey:@"ip"]
+        [connCtrlr connectToHostname:[serverItem objectForKey:@"ip"]
                                port:[[serverItem objectForKey:@"port"] intValue]
                        withUsername:[[alertView textFieldAtIndex:0] text]
                         andPassword:nil
