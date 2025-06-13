e3bc0e25d532c7b03da691a34512e8b838d0414a
0n1cOn3 <0n1cOn3@gmx.ch>
2025-06-13 04:57:51 +0000

    Replace Objective-C badges and base64 libs with Swift

--- Source/Classes/MUDataURL.m
+++ Source/Classes/MUDataURL.m
@@ -3,14 +3,12 @@
 // license that can be found in the LICENSE file.
 
 #import "MUDataURL.h"
-#import "GTMStringEncoding.h"
 
 @implementation MUDataURL
 
 // todo(mkrautz): Redo this with our own internal scanning and base64 decoding
 // to get rid of the string copying.
 + (NSData *) dataFromDataURL:(NSString *)dataURL {
-    GTMStringEncoding *base64decoder = [GTMStringEncoding rfc4648Base64StringEncoding];
 
     // Read: data:<mimetype>;<encoding>,<data>
     // Expect encoding = base64
@@ -33,7 +31,8 @@
     NSString *base64data = [mimeStr substringFromIndex:r.location+r.length];
     base64data = [base64data stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
     base64data = [base64data stringByReplacingOccurrencesOfString:@" " withString:@""];
-    return [base64decoder decode:base64data];
+    NSData *data = [[NSData alloc] initWithBase64EncodedString:base64data options:0];
+    return data;
 }
 
 + (UIImage *) imageFromDataURL:(NSString *)dataURL {
