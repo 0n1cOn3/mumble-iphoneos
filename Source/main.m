84cf8bec6b2512ce9ab080209136d2448737a9b9
0n1cOn3 <0n1cOn3@gmx.ch>
2025-06-13 08:47:35 +0200

    Fixing some PR issues from in before

--- Source/main.m
+++ Source/main.m
@@ -1,9 +1,10 @@
-// Copyright 2009-2010 The 'Mumble for iOS' Developers. All rights reserved.
+// Copyright 2009-2010, The 'Mumble for iOS' Developers. All rights reserved.
+// Copyright 2025, 0n1cOn3. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 #import <UIKit/UIKit.h>
-#import "Classes/MUApplicationDelegate.h"
+#import "MUApplicationDelegate.h"
 
 int main(int argc, char *argv[]) {
     @autoreleasepool {
