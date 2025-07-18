eb8ed1ed97e913a3c8d8d68b2d2c65c56aa27f45
0n1cOn3 <0n1cOn3@gmx.ch>
2025-07-18 05:34:11 +0200

    Add Swift bridging header

---
+++ Source/Mumble-Bridging-Header.h
@@ -0,0 +1,9 @@
+#import <MumbleKit/MumbleKit.h>
+
+// Expose Objective-C headers to Swift
+#import "MUColor.h"
+#import "MUImage.h"
+#import "MUAudioBarView.h"
+
+// Add further imports as Swift files are added
+
